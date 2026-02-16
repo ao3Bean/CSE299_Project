da9c01a1c3ad4a3c0fc17f119edbc9f82f43efbd
ao3Bean <nabila.mahjabinnoor@gmail.com>
2026-02-17 00:25:07 +0600

    linked home page and login

--- support/core/urls.py
+++ support/core/urls.py
@@ -6,6 +6,7 @@ from . import views
 #list for all the urls that the user can navigate from core app 
 urlpatterns = [  
     path('', views.index, name='index'), #when the url is empty, go to the index view
+    path('login/', views.login, name='login'), #when the url is empty, go to the index view
     #path('url_name/', views.view_function_name, name='url_name')
     #so from '' it goes to index than we can redirect to v1/
 ]
