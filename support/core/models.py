4c3cea36bdc38148523fc6439e7ef5f4b6803ce6
ananya0511sarkar-prog <ananya0511sarkar@gmail.com>
2026-02-21 21:47:52 +0600

    add user profile , django models

--- support/core/models.py
+++ support/core/models.py
@@ -1,3 +1,40 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    hair = models.CharField(max_length=50, default="Default Hair")
+
+    face_expression = models.CharField(max_length=50, default="Neutral")
+
+    clothes = models.CharField(max_length=50, default="Default Clothes")
+
+    skin_color = models.CharField(max_length=50, default="Medium")
+
+    def __str__(self):
+        return self.user.username
+
+
+#for the chat room model: 
+class Room(models.Model):
+
+    ROOM_TYPE_CHOICES = [
+        ('public', 'Public'),
+        ('private', 'Private'),
+    ]
+
+    name = models.CharField(max_length=100)
+
+    room_type = models.CharField(
+        max_length=10,
+        choices=ROOM_TYPE_CHOICES,
+        default='public'
+    )
+
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 # Create your models here.
--- support/core/views.py
+++ support/core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+
 # Create your views here.
 def index(request):
     return render (request, "index.html")
@@ -10,3 +12,7 @@ def login(request):
 
 def user_dashboard(request):
     return render(request, "user_dashboard.html")
+
+def user_profile(request):
+    return render(request, "user_profile.html")
+
