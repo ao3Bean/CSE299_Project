34abdfca1aaa3e2735ae5d50752cf0f37028fe35
Nazia Faruque Diya <diya.nazia2@gmail.com>
2026-02-24 03:15:22 +0600

    Added Room model with UUID and expires_at

--- support/room/models.py
+++ support/room/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+import uuid
+from datetime import timedelta
 
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
+
+
+class Room(models.Model):
+    ROOM_TYPE_CHOICES = [
+        ('public', 'Public'),
+        ('private', 'Private'),
+    ]
+
+    room_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    name = models.CharField(max_length=100)
+
+    room_type = models.CharField(
+        max_length=10,
+        choices=ROOM_TYPE_CHOICES,
+        default='public'
+    )
+
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    expires_at = models.DateTimeField(null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.expires_at:
+            self.expires_at = timezone.now() + timedelta(hours=2)  # adjust if needed
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
