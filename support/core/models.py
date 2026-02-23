c78a025593fdf1dac491796a44944d6ec2f0ea89
Nazia Faruque Diya <diya.nazia2@gmail.com>
2026-02-24 03:18:52 +0600

    Removed Room model from core app

--- support/core/models.py
+++ support/core/models.py
@@ -17,24 +17,3 @@ class UserProfile(models.Model):
         return self.user.username
 
 
-#for the chat room model: 
-class Room(models.Model):
-
-    ROOM_TYPE_CHOICES = [
-        ('public', 'Public'),
-        ('private', 'Private'),
-    ]
-
-    name = models.CharField(max_length=100)
-
-    room_type = models.CharField(
-        max_length=10,
-        choices=ROOM_TYPE_CHOICES,
-        default='public'
-    )
-
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
-# Create your models here.
