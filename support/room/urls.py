94d9973fe5a7b95c71222fae27d7668021677bed
Nazia Faruque Diya <diya.nazia2@gmail.com>
2026-02-25 00:41:23 +0600

    Added room UUID route

--- support/room/urls.py
+++ support/room/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('rooms/', views.rooms, name='rooms'),
+    path('rooms/<uuid:room_id>/', views.room_detail, name='room_detail'), 
     path('community/', views.community, name='community'),
     path('chatroom/', views.chatroom, name='chatroom'),
 ]
