9251ea6477cd3b4f5bf09dee0bd6e963dffaa80c
ao3Bean <nabila.mahjabinnoor@gmail.com>
2026-02-26 22:15:56 +0600

    added room models : room, message, session

--- support/room/models.py
+++ support/room/models.py
@@ -1,35 +1,57 @@
 from django.db import models
 import uuid
-from datetime import timedelta
 
 from django.contrib.auth.models import User
-from django.db import models
 from django.utils import timezone
+from datetime import timedelta
 
 
 class Room(models.Model):
-    ROOM_TYPE_CHOICES = [
-        ('public', 'Public'),
-        ('private', 'Private'),
-    ]
-
+    #will stay default for all rooms, but can be changed by host when saving rooms
     room_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200, blank=True, default="")
+    host = models.ForeignKey(User, on_delete=models.CASCADE, related_name="hosted_rooms")
+    is_private = models.BooleanField(default=False)
+    passcode = models.CharField(max_length=20, blank=True, null=True)
 
-    room_type = models.CharField(
-        max_length=10,
-        choices=ROOM_TYPE_CHOICES,
-        default='public'
-    )
-
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
-
+    #user customizable settings, will stay default for all rooms, but can be changed by host when saving rooms
+    background_preset = models.CharField(max_length=50, default="preset1")
+    focus_duration = models.IntegerField(default=25)
+    break_duration = models.IntegerField(default=5)
+    is_saved = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
     expires_at = models.DateTimeField(null=True, blank=True)
+    max_participants = models.IntegerField(default=8)
 
     def save(self, *args, **kwargs):
         if not self.expires_at:
-            self.expires_at = timezone.now() + timedelta(hours=2)  # adjust if needed
+            self.expires_at = timezone.now() + timedelta(hours=24)
         super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
+
+
+#for user chatting
+class Message(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name="messages")
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(max_length=500)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["timestamp"]
+
+    def __str__(self):
+        return f"{self.sender.username}: {self.content[:40]}"
+
+
+class Session(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name="sessions")
+    start_time = models.DateTimeField(auto_now_add=True)
+    end_time = models.DateTimeField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"Session in {self.room.name}"
